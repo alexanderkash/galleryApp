5033268b8d5892c65c908c2d45ce287dda359671
Alex Kogalovsky <extrakash@gmail.com>
2021-11-15 01:12:57 +0300

    some code has been refactored

--- GalleryApp/NetworkServices/PhotoInfoLoader.swift
+++ GalleryApp/NetworkServices/PhotoInfoLoader.swift
@@ -14,21 +14,21 @@ struct PhotoInfoLoader {
     func loadPhotosInfo(completion: @escaping (Result<[Photo], Error>) -> Void) {
         guard let url = URL(string: photosInfoUrl) else { return }
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            DispatchQueue.main.async {
-                if let error = error {
-                    print(error.localizedDescription)
-                    completion(.failure(error))
-                    return
-                }
-                guard let data = data else { return }
-                do {
-                    let result = try JSONDecoder().decode(DecodedArray<Photo>.self, from: data)
-                    let photos = result.compactMap{ $0 }
+            if let error = error {
+                print(error.localizedDescription)
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else { return }
+            do {
+                let result = try JSONDecoder().decode(DecodedArray<Photo>.self, from: data)
+                let photos = result.compactMap{ $0 }
+                DispatchQueue.main.async {
                     completion(.success(photos))
-                } catch let jsonError {
-                    print("Failed to decode JSON", jsonError.localizedDescription)
-                    completion(.failure(jsonError))
                 }
+            } catch let jsonError {
+                print("Failed to decode JSON", jsonError.localizedDescription)
+                completion(.failure(jsonError))
             }
         }
         task.resume()
